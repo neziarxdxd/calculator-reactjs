5c7bdcb7dda3c53954e4e1468a14153bcbd5528b
raiiiz <rickdavidsangalang@gmail.com>
2021-10-17 10:54:33 +0800

    Updates

---
+++ precache-manifest.f0213b224ea9ecce6adbf3393ef36424.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce05ffe773a8bd886d67ac050d73548",
+    "url": "/calculator-reactjs/index.html"
+  },
+  {
+    "revision": "ed22b2c13ffe3539d32c",
+    "url": "/calculator-reactjs/static/css/main.78fd6f91.chunk.css"
+  },
+  {
+    "revision": "bf446e5b45ce303bd220",
+    "url": "/calculator-reactjs/static/js/2.12436241.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-reactjs/static/js/2.12436241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed22b2c13ffe3539d32c",
+    "url": "/calculator-reactjs/static/js/main.55195579.chunk.js"
+  },
+  {
+    "revision": "e0218c6651db93805e3f",
+    "url": "/calculator-reactjs/static/js/runtime-main.42eb987b.js"
+  }
+]);
