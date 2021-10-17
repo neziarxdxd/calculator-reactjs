386e022a91b295c5fa2eff8c917a8047c420ef93
raiiiz <rickdavidsangalang@gmail.com>
2021-10-17 10:37:34 +0800

    initial

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import MyButtons from './layouts/Button';
+import Answer from './layouts/Answer'
+import './App.css'
+
+
+class App extends Component{
+  state ={
+    answer:''
+  } 
+  buttonPress = buttonName =>{
+    if (buttonName==='='){
+      this.calculate()
+    }
+    else if(buttonName==='C'){
+      this.reset()
+    }
+    else if(buttonName==='CE'){
+      this.backspace()
+    }  
+    else{
+      
+    this.setState({
+      answer:this.state.answer + buttonName
+    });
+    }
+  }     
+  calculate =()=>{
+    try{
+    this.setState({
+        answer:(eval(this.state.answer) || "") +""
+
+    });}
+    catch{
+      this.setState({
+        answer:"error"
+      })
+      
+    }
+  }
+  backspace =()=>{
+    this.setState({
+        answer:this.state.answer.slice(0,-1)
+
+    });
+  }
+
+  reset =()=>{
+    this.setState({
+        answer:""
+
+    });
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1 className="title"> CALCULATOR</h1>
+        <Answer answer={this.state.answer}/>
+        <MyButtons buttonPress={this.buttonPress}/>
+        <div>
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
